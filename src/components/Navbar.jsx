c5f1c92ee65155301888bf60abae3552f03c981b
Valentin Sierra <valennsiera@gmail.com>
2024-11-30 12:02:55 -0300

    añadiendo categorias

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import CartWidget from './CartWidget';
+import { RiArrowDownSFill } from "react-icons/ri";
 
 const Navbar = () =>{
     return(
+        <>
         <header className='h-[10vh] text-gray-300 flex items-center justify-between py-4 px-10 z-40'>
             {/* Menu nav */}
             <nav className='flex items-center '>
@@ -25,6 +27,28 @@ const Navbar = () =>{
             {/* Cart */}
             <CartWidget />
         </header>
+        <div className='text-gray-300 py-2 px-10 border-t border-gray-600'>
+            <h3 className='font-semibold text-xl mb-1'>Categories</h3>
+            <ul className='flex items-center gap-6 py-2'>
+                <li className='flex items-center gap-1 hover:text-[#e58d27] transition-colors'>
+                    <a href="#">Playstation</a>
+                    <RiArrowDownSFill />
+                </li>
+                <li className='flex items-center gap-1 hover:text-[#e58d27] transition-colors'>
+                    <a href="#">Xbox</a>
+                    <RiArrowDownSFill />
+                </li>
+                <li className='flex items-center gap-1 hover:text-[#e58d27] transition-colors'>
+                    <a href="#">Nintendo</a>
+                    <RiArrowDownSFill />
+                </li>
+                <li className='flex items-center gap-1 hover:text-[#e58d27] transition-colors'>
+                    <a href="#">Pc</a>
+                    <RiArrowDownSFill />
+                </li>
+            </ul>
+        </div>
+        </>
     )
 }
 
